e95d16a8c7c9d5791f3ce0209f01de98ac39f19d
Mary Notari <32004486+marynotari@users.noreply.github.com>
2018-04-22 14:42:11 -0400

    add black rain slider with bkm vid

---
+++ 02_Choreo_p5__Black_Rain_Slider_2018_04_23/sketch.js
@@ -0,0 +1,92 @@
+//NOTE: I attempted to create a separate file within project-folder for function rainDrop().
+//			It isn't working currently.
+
+// global variables
+let drops = [];
+let sliderRain;
+let wind;
+let vid;
+let img;
+
+
+function setup() {
+  // create image background
+  // img = loadImage("placeholder.png");
+
+  // create video background
+  vid = createVideo("contour_large.mp4");
+  vid.hide(); //vid shows up as separate DOM element next to the canvas, so hide it and then redraw below
+
+  createCanvas(1920, 1080);
+
+  // create slider to control number of raindrops
+  sliderRain = createSlider(0, 2000, 0);
+  sliderRain.position(10, 10);
+  sliderRain.size(width-20);
+
+  // create a new raindrop for each loop
+  // push the raindrop to the empty array defined above
+  // max # of raindrops defined as 2000
+  for (let i = 0; i < 2000; i++) {
+    drops[i] = new rainDrop();
+  }
+}
+
+function draw() {
+  background(255);
+  // background(img);
+  // background(vid);
+
+  image(vid, 0, 0);
+
+  //make slider human controled somehow
+  for (i = 0; i < sliderRain.value(); i++) {
+    drops[i].show();
+    drops[i].move();
+  }
+}
+
+function mousePressed() {
+  vid.loop();
+}
+
+class rainDrop {
+
+  //Declare the local variables that define the raindrops' starting points
+  constructor() {
+    this.x = random(width);
+    this.y = random(-500, -10);
+    this.z = random(0, 20);
+    this.size = map(this.z, 0, 20, 5, 20);
+    this.speed = map(this.z, 0, 20, 3, 10);
+
+    //Declare a variable for the wind--see applyForce() below
+    this.blow = createVector();
+
+    //Declare a gravity variable that obeys the z-axis rules above
+    this.grav = map(this.z, 0, 20, 0.025, 0.2);
+
+  }
+
+  // Mimic a z-axis so drops appear closer or farther away:
+  // Declare a z variable
+  // Map the z variable to size and speed to so that the smaller #s goes from shortest to longest and slowest to fastest
+  show() {
+    //Draw the raindrop
+    stroke(0);
+    strokeWeight(map(this.z, 0.1, 20, 1, 2));
+    line(this.x, this.y, this.x, this.y + this.size);
+  }
+
+  //Create the motion of the raindrop
+  move() {
+    this.y = this.y + this.speed;
+    this.speed = this.speed + this.grav //speed increases slightly as it falls to mimick gravity
+
+    // After the raindrop hits the bottom, it resets to its starting point and with its starting speed
+    if (this.y > height) {
+      this.y = random(-200, -10);
+      this.speed = map(this.z, 0, 20, 4, 10);
+    }
+  }
+}
